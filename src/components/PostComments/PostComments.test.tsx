bdce6b6d9bd6cfebaeb89a62e71fa5ae02d4b79c
igor <igor.oliveira092005@gmail.com>
2025-11-11 15:05:17 -0300

    adição teste para verificar o envio de 2 comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,19 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import PostComment from '.'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+  test('Devem ser inseridos 2 comentários', () => {
+    render(<PostComment />)
+
+    const botao = screen.getByTestId('btn-comentar')
+    fireEvent.click(botao)
+    fireEvent.click(botao)
+
+    const comentarios = screen.getAllByTestId('comentario')
+    expect(comentarios).toHaveLength(2)
+  })
+})
